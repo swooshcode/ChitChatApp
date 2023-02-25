a347f04bfb20e527a9942c3e137c27b0eaaae966
黃龍 <115655242+swooshcode@users.noreply.github.com>
2023-02-25 02:36:41 -0500

    Push notifications added

--- Source-Code/index.js
+++ Source-Code/index.js
@@ -3,10 +3,25 @@
  */
 
 import { AppRegistry } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { firebase } from '@react-native-firebase/app';
 import App from './App';
 import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
-import messaging from '@react-native-firebase/messaging';
 
-  
+// Initialize Firebase
+const firebaseConfig = {
+  // Your Firebase config here
+};
+firebase.initializeApp(firebaseConfig);
+
+// Request user permission for notifications
+messaging().requestPermission().then(() => {
+  console.log('Permission granted!');
+}).catch(error => {
+  console.log('Permission denied:', error);
+});
+
 AppRegistry.registerComponent(appName, () => App);
+
+
